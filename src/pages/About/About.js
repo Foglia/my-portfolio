d9f5d5f9f4fb52428e679a651a7a9b603f8901b2
Flavia Fogliato <flavia.fogliato@gmail.com>
2023-05-01 19:03:47 +0100

    about me text

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -26,9 +26,10 @@ function About() {
         To me, a great website is more than just functionality - it's about creating an experience that users will love and remember.
         I'm passionate about web development because it allows me to solve problems and make a difference through my work. Whether you're looking to launch a new website, update an existing one, or just need some advice on how to improve your online presence, I'm here to help.
         <br />
-        When I'm not tinkering with code, you can find me exploring the local coffee scene, trying out new recipes in the kitchen, or immersing myself in the local arts and culture scene. I also believe in the power of technology for good, and I'm always looking for ways to make a positive impact through my work.
+        When I'm not tinkering with code, you can find me exploring local coffee, trying out new recipes in the kitchen, or immersing myself in the local arts and culture scene. 
         <br />
-        I believe that technology can be a powerful tool for positive change.</p>
+        I also believe in the power of technology for good, and I'm always looking for ways to make a positive impact through my work.
+        </p>
       <div className='aboutButtons'>
         <StyledButton onClick={() => window.open("https://www.linkedin.com/in/flaviafogliato/", "_blank")} className='button1'>LinkedIn</StyledButton>    
         <StyledButton2 onClick={() => window.open("https://github.com/Foglia", "_blank")} className='button1'>Github</StyledButton2>    
