6f3d793c26b88d26696a6e3fdbd07c8eb61f5700
Flavia Fogliato <flavia.fogliato@gmail.com>
2023-06-07 19:12:54 +0100

    2

--- src/components/Circle/Circle.jsx
+++ src/components/Circle/Circle.jsx
@@ -5,7 +5,7 @@ function Circle() {
   const rotateText = () => {
     const text = "A random animation . Because everyone loves it .";
     return text.split("").map((char, i) => (
-      <span key={i} style={{ transform: `rotate(${i * 7.4}deg)` }}>{char}</span>
+      <span key={i} style={{ transform: `rotate(${i * 7.4}deg)` }} id='spamCircle'>{char}</span>
     ));
    }
 
